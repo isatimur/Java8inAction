7db4715f3e9e06fd27c80b0b5cc92b2cf955ccb4
isatimur <isatimur.it@gmail.com>
2016-01-18 03:21:27 +0300

    Chapter 4  -  Streams - reduce function

---
+++ src/com/isatimur/StreamReduceF.java
@@ -0,0 +1,22 @@
+package com.isatimur;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+/**
+ * Created by tisachenko on 18.01.2016.
+ */
+public class StreamReduceF {
+    public static void main(String[] args) {
+        IntStream integers = IntStream.range(1, 100);
+//        int sum = integers.sum();
+        int sum = integers.reduce(0, (a, b) -> (a + b));
+
+        System.out.println("Summary of first hundread int elements: " + sum);
+
+        LongStream longStream = LongStream.range(1, 50);
+        long factorial = longStream.reduce(1, (a, b) -> (a * b));
+
+        System.out.println("Factorial of first fifty int elements: " + factorial);
+    }
+}
