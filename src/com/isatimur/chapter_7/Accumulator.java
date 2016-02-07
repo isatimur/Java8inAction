68073045e6fa4f81e309b31a2d685b58ad011d03
Исаченко Тимур Игоревич <tisachenko@at-consulting.ru>
2016-02-07 21:05:57 +0300

    Added a new chapter 7

---
+++ src/com/isatimur/chapter_7/Accumulator.java
@@ -0,0 +1,15 @@
+package com.isatimur.chapter_7;
+
+/**
+ * Created by tisachenko on 07.02.16.
+ */
+public class Accumulator {
+
+    public long total = 0;
+
+    public void add(long value) {
+        total += value;
+    }
+
+
+}
